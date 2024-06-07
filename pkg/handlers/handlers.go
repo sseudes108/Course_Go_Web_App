f6a6be2a64a6c505cd4fd3162ef5d355551f1b58
Eudes <senstur.art@gmail.com>
2024-06-06 22:45:58 -0300

    start format go templates fromm html

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -30,22 +30,30 @@ func NewHandlers(repo *Repository) {
 
 // Home is the about page handler
 func (repo *Repository) Home(w http.ResponseWriter, r *http.Request) {
-
-	remoteIP := r.RemoteAddr
-	repo.App.Session.Put(r.Context(), "remote_ip", remoteIP)
-
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the about page handler
 func (repo *Repository) About(w http.ResponseWriter, r *http.Request) {
-	stringMap := make(map[string]string)
-	stringMap["test"] = "Hello, again."
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{})
+}
 
-	remoteIP := repo.App.Session.GetString(r.Context(), "remote_ip")
-	stringMap["remote_ip"] = remoteIP
+func (repo *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
+}
 
-	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
-		StringMap: stringMap,
-	})
+func (repo *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+}
+
+func (repo *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+
+func (repo *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
+}
+
+func (repo *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
 }
